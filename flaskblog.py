c5adcb95ccd93fcba94d7f8d6a50c575cee5d759
Ankit Chourasiya <ankitchourasiya94@gmail.com>
2021-07-14 02:04:33 +0530

    Created database model in flaskblog.py file

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,43 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from datetime import datetime
+from flask import Flask, config, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 from forms import RegistartionForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '24ee0c10cc86e98d7eb9fcc08611d46b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # Here we are reffrenceing the Post class that by P is Capital and backref argunet print all user details
+    # as a auther when we write post.author
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # this method responsable for How our object is printed whenever we print it out
+    def __repr__(self) -> str:
+        return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # Here we actually refrencing the table name and column name so thats by user.id is in Lower case by default
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post('{self.title}','{self.date_posted}')"
+
 
 post = [
     {
