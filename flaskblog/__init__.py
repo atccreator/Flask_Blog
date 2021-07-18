e431f27aaab06ad85c4f72e152e9976a1c949d5c
Ankit Chourasiya <ankitchourasiya94@gmail.com>
2021-07-18 23:45:08 +0530

    login and logout functionality completd

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login.utils import login_required
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import form
 from sqlalchemy.orm import backref
@@ -11,5 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
